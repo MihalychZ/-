83859d9c340158d29d74b6639ab2315f73c056bb
MihalychZ <tonitolstoi1337@yandex.ru>
2024-05-21 22:33:21 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -9,18 +9,18 @@ int main()
     scanf("%d", &c);
     switch(c){
     case 1: return laba1();
-    case 2: return dop1();
-    case 3: return laba2();
-    case 4: return dop2();
-    case 5: return laba3();
-    case 6: return dop3();
-    case 7: return laba4();
-    case 8: return dop4();
-    case 9: return laba5();
-    case 10: return dop5();
-    case 11: return laba6();
-    case 12: return dop6();
-    case 13: return laba7();
+    case 11: return dop1();
+    case 2: return laba2();
+    case 22: return dop2();
+    case 3: return laba3();
+    case 33: return dop3();
+    case 4: return laba4();
+    case 44: return dop4();
+    case 5: return laba5();
+    case 55: return dop5();
+    case 6: return laba6();
+    case 66: return dop6();
+    case 7: return laba7();
     }
 
     return 0;
